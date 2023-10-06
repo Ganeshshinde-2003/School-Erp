72ff92b69912eb09252f4876f83c6e6f6b4a1730
Suraj Poddar <surajku2022@gmail.com>
2023-10-06 17:58:56 +0530

    added attendance table in home page

---
+++ app/src/Database/Attendance.js
@@ -0,0 +1,32 @@
+const attendance = [
+  {
+    "SI.No": 1,
+    Class: "VII",
+    Sec: "A",
+    Present: 14,
+    Absent: 19,
+  },
+  {
+    "SI.No": 2,
+    Class: "V",
+    Sec: "A",
+    Present: 10,
+    Absent: 3,
+  },
+  {
+    "SI.No": 3,
+    Class: "X",
+    Sec: "D",
+    Present: 20,
+    Absent: 9,
+  },
+  {
+    "SI.No": 4,
+    Class: "IX",
+    Sec: "C",
+    Present: 30,
+    Absent: 3,
+  },
+];
+
+export default attendance;
--- app/src/Pages/Home/Home.js
+++ app/src/Pages/Home/Home.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Birthdays from "../../Database/Birthday";
 import DynamicTable from "../../Components/DynamicTable";
+import Attendance from "../../Database/Attendance";
 import "./Home.css";
 const Home = () => {
   return (
     <div className="mt-5">
-      <div className="birthdays">
-        <h1 className="text-center font-bold text-white	">Today's Birthday's</h1>
-        <DynamicTable data={Birthdays} />
+      <div className="flex justify-around">
+        <div className="birthdays-table">
+          <h1 className="text-center font-bold text-white	">
+            Today's Birthday's
+          </h1>
+          <DynamicTable data={Birthdays} />
+        </div>
+        <div className="attendance-table">
+          <h1 className="text-center font-bold text-white	">Attendance Count</h1>
+          <DynamicTable data={Attendance} />
+        </div>
       </div>
     </div>
   );
