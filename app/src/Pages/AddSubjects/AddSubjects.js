edb6511f1982d82834a174ee76bef459a000a18d
Nuthan B <NuthanB@protonmail.com>
2023-10-23 18:14:32 +0530

    Integrated the API for add subjects

--- app/src/Pages/AddSubjects/AddSubjects.js
+++ app/src/Pages/AddSubjects/AddSubjects.js
@@ -1,41 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import AddSubjectData from "../../Database/AddSubjects";
 import DynamicTable from "../../Components/DynamicTable";
-import "./AddSubject.css";
 import AddButton from "../../Components/AddButton";
-import addSubjectToDatabase from "../../api/subject";
+import Modal from "../../Components/AddSubject_Modal"; // Import the Modal component
+import "./AddSubject.css";
+
 const AddSubject = () => {
-  
-  // this is a sample object foermate that should be pass to the addSubjectToDatabase(object:subjectData)
-  const subjectData = {
-    className: "6", 
-    subjectName: "Mathematics", 
-    subjectId: "1012", 
-};
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [subjectAdded, setSubjectAdded] = useState(false);
+
+  // Function to open the modal
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleSubjectAdded = () => {
+    setSubjectAdded(true);
+    setTimeout(() => {
+      setSubjectAdded(false);
+    }, 2000); // Hide the message after 2 seconds
+  };
 
   return (
     <div className="mt-4 w-full">
-      {/* <h1 className="text-center"> Add Subjects</h1> */}
-      <div className="mt-5 max-w- min-w-full">
+      <div className="mt-5 max-w-full">
         <div className="flex justify-around">
           <div className="add-optional-sub-table">
             <h1 className="h-16 text-center font-bold text-white flex items-center justify-center">
               Add Subjects
             </h1>
-            <DynamicTable
-              data={AddSubjectData}
-              rowHeight={100}
-              action={true}
-            />
+            <DynamicTable data={AddSubjectData} rowHeight={100} action={true} />
             <p className="h-16 text-center font-bold text-white flex items-center justify-center">
-            <AddButton buttonText={"Add subject"}  onClickButton={() => addSubjectToDatabase(subjectData)} />
+              <AddButton buttonText={"Add subject"} onClickButton={openModal} />
             </p>
           </div>
         </div>
       </div>
+      <Modal isOpen={isModalOpen} onClose={closeModal} onSubjectAdded={handleSubjectAdded} />
+      {subjectAdded && (
+        <div className="text-green-500 text-center mt-2">Subject has been successfully added!</div>
+      )}
     </div>
   );
 };
 
 export default AddSubject;
-
