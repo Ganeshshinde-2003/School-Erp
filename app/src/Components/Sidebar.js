aa45a08a4034038cac4df1bd7d8a5f092c57acb7
Nuthan B <NuthanB@protonmail.com>
2023-11-07 01:46:53 +0530

    Made the button changes and color

--- app/src/Components/Sidebar.js
+++ app/src/Components/Sidebar.js
@@ -1,46 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
 import { SidebarData } from "../Database/SidebarData";
 import { IconContext } from "react-icons/lib";
 import SubMenu from "./SubMenu";
 
 const SidebarNav = styled.nav`
-  background-image: linear-gradient( #333333,  #333333);
+  background-image: linear-gradient(#333333, #333333);
   height: 100vh;
   display: flex;
-  border: 1px solid #333333;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
   position: sticky;
   top: 2.7rem;
   z-index: 10;
   overflow: auto;
   width: 25rem;
+  border: none; /* Remove the border */
+  padding: 0; /* Remove padding */
 `;
 
 const SidebarWrap = styled.div`
   width: 100%;
-  broder : 1px solid #333333;
 `;
 
 const Sidebar = () => {
   return (
-    <>
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <SidebarNav>
-          <SidebarWrap>
-            <div>
-              <img src="assets/images/logo.png" className="w-full	" />
-            </div>
-            {SidebarData.map((item, index) => {
-              return <SubMenu item={item} key={index} />;
-            })}
-          </SidebarWrap>
-        </SidebarNav>
-      </IconContext.Provider>
-    </>
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <SidebarNav>
+        <SidebarWrap>
+          <div>
+            <img src="assets/images/logo.png" className="w-full" alt="Logo" />
+          </div>
+          {SidebarData.map((item, index) => (
+            <SubMenu item={item} key={index} />
+          ))}
+        </SidebarWrap>
+      </SidebarNav>
+    </IconContext.Provider>
   );
 };
 
