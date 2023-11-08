4dbe5f9070bc789a0aea0bd330c49d9596fe75a5
Amar kumar <aknbo30@gmail.com>
2023-11-08 19:12:57 +0530

    addTeacher update & get specificdata apis created

--- app/src/api/TeacherMaster/AddTeacher.js
+++ app/src/api/TeacherMaster/AddTeacher.js
@@ -1,16 +1,5 @@
 import { db } from "../../config/firebase";
-import {
-  getDocs,
-  addDoc,
-  doc,
-  updateDoc,
-  deleteDoc,
-  collection,
-  getDoc,
-  serverTimestamp,
-  query,
-  orderBy, // import serverTimestamp
-} from "firebase/firestore";
+import { getDocs,addDoc,doc,updateDoc,deleteDoc,collection,getDoc,serverTimestamp,query,orderBy,setDoc, } from "firebase/firestore";
 
 /**
  * Add a teacher to the database with subcollections.
@@ -23,175 +12,161 @@ import {
  * @param {number} teacherData.mobileNo - The mobile number of the teacher.
  * @param {array} teacherData.classTeacher - An array of class names associated with the teacher.
  * @param {string} teacherData.transportSlab - The transport slab information.
+ * @param {map} teacherData.personalDetailsData - The transport slab information.
+ * @param {map} teacherData.salaryDetailsData - The transport slab information.
+ * @param {map} teacherData.addressDetailsData - The transport slab information.
+ * @param {map} teacherData.experienceDetailsData - The transport slab information.
+
  */
 
-// const teacherDatatest = {
-//     teacherId: "th102", // Updated teacher ID
-//     designation: "Subject Teacher", // Updated designation
-//     emailId: "testteacher@gmail.com", // Updated email
-//     firstName: "John", // Updated first name
-//     lastName: "Smith", // Updated last name
-//     mobileNo: 9876543210, // Updated mobile number
-//     classTeacher: ["Class C", "Class D"], // Updated class teacher assignments
-//     transportSlab: "bus", // Updated transport slab
+export const teacherDatatest = {
+    teacherId: "th102", // Updated teacher ID
+    designation: "Subject Teacher", // Updated designation
+    emailId: "testteacher@gmail.com", // Updated email
+    firstName: "John", // Updated first name
+    lastName: "Smith", // Updated last name
+    mobileNo: 9876543210, // Updated mobile number
+    classTeacher: ["Class C", "Class D"], // Updated class teacher assignments
+    transportSlab: "bus", // Updated transport slab
 
-//     personalDetailsData: {
-//         dob: new Date(1990, 5, 15), // Updated date of birth
-//         fatherName: "John Sr.", // Updated father's name
-//         motherName: "Mary", // Updated mother's name
-//         spouseName: "Jane", // Updated spouse's name
-//         sex: "female", // Updated gender
-//         cast: "New Cast", // Updated cast
-//         castCategory: "OBC", // Updated cast category
-//         bloodGroup: "B-", // Updated blood group
-//     },
+    personalDetailsData: {
+        dob: new Date(1990, 5, 15), // Updated date of birth
+        fatherName: "John Sr.", // Updated father's name
+        motherName: "Mary", // Updated mother's name
+        spouseName: "Jane", // Updated spouse's name
+        sex: "female", // Updated gender
+        cast: "New Cast", // Updated cast
+        castCategory: "OBC", // Updated cast category
+        bloodGroup: "B-", // Updated blood group
+    },
 
-//     addressDetailsData: {
-//         address: "456 Elm Street", // Updated address
-//         city: "Another City", // Updated city
-//         zipCode: "54321", // Updated ZIP code
-//         state: "Another State", // Updated state
-//         homeTelephoneNo: "9876123456", // Updated home telephone number
-//     },
+    addressDetailsData: {
+        address: "456 Elm Street", // Updated address
+        city: "Another City", // Updated city
+        zipCode: "54321", // Updated ZIP code
+        state: "Another State", // Updated state
+        homeTelephoneNo: "9876123456", // Updated home telephone number
+    },
 
-//     salaryDetailsData: {
-//         basic: "abcxyz", // Updated basic salary
-//         acNo: 9876543210, // Updated account number
-//         lic: "xyzabc", // Updated LIC policy
-//         loan: "7500", // Updated loan amount
-//         pfApplied: "def", // Updated PF applied status
-//         pfNo: 9876543210, // Updated PF number
-//         previousYearSalary: 135792, // Updated previous year salary
-//         salaryAmount: 250000, // Updated salary amount
-//     },
+    salaryDetailsData: {
+        basic: "abcxyz", // Updated basic salary
+        acNo: 9876543210, // Updated account number
+        lic: "xyzabc", // Updated LIC policy
+        loan: "7500", // Updated loan amount
+        pfApplied: "def", // Updated PF applied status
+        pfNo: 9876543210, // Updated PF number
+        previousYearSalary: 135792, // Updated previous year salary
+        salaryAmount: 250000, // Updated salary amount
+    },
 
-//     experienceDetailsData: {
-//         completionYear: "2019", // Updated completion year
-//         joiningDate: new Date(2017, 2, 10), // Updated joining date
-//         serviceInYears: 4, // Updated years of service
-//         confirmationDate: new Date(2017, 6, 20), // Updated confirmation date
-//         experienceSummary: "New job experience summary", // Updated experience summary
-//         oldPFNo: "987654", // Updated old PF number
-//         previousJob: "New Job Title", // Updated previous job title
-//         dateOfLeaving: new Date(2019, 10, 15), // Updated date of leaving
-//         dateOfConfirmation: new Date(2017, 8, 1), // Updated date of confirmation
-//         lastJobSalary: 60000, // Updated last job salary
-//         reasonForLeaving: "Personal reasons", // Updated reason for leaving
-//     },
-// };
+    experienceDetailsData: {
+        completionYear: "2019", // Updated completion year
+        joiningDate: new Date(2017, 2, 10), // Updated joining date
+        serviceInYears: 4, // Updated years of service
+        confirmationDate: new Date(2017, 6, 20), // Updated confirmation date
+        experienceSummary: "New job experience summary", // Updated experience summary
+        oldPFNo: "987654", // Updated old PF number
+        previousJob: "New Job Title", // Updated previous job title
+        dateOfLeaving: new Date(2019, 10, 15), // Updated date of leaving
+        dateOfConfirmation: new Date(2017, 8, 1), // Updated date of confirmation
+        lastJobSalary: 60000, // Updated last job salary
+        reasonForLeaving: "Personal reasons", // Updated reason for leaving
+    },
+};
 
 export const addTeacherToDatabase = async (teacherData) => {
-  console.log(teacherData);
 
-  const teacherRef = collection(db, "AddTeachers");
-  try {
-    const teacherDoc = await addDoc(teacherRef, {
-      teacherId: teacherData.teacherId,
-      designation: teacherData.designation,
-      emailId: teacherData.emailId,
-      firstName: teacherData.firstName,
-      lastName: teacherData.lastName,
-      mobileNo: teacherData.mobileNo,
-      classTeacher: teacherData.classTeacher,
-      transportSlab: teacherData.transportSlab,
-      createdAt: serverTimestamp(),
-    });
-
-    // Create subcollections within the teacher document
-    const personalDetailsRef = collection(teacherDoc, "PersonalDetails");
-    const addressDetailsRef = collection(teacherDoc, "AddressDetails");
-    const salaryDetailsRef = collection(teacherDoc, "SalaryDetails");
-    const experienceDetailsRef = collection(teacherDoc, "ExperienceDetails");
-
-    // Add data to subcollections (modify with actual data as needed)
-    // Personal Details data
+    const teacherRef = collection(db, "AddTeachers");
     try {
-      await addDoc(addressDetailsRef, teacherData?.addressDetailsData);
-    } catch (error) {
-      console.error("Error adding AddressDetails: ", error);
-    }
+        const teacherDoc = await addDoc(teacherRef, {
+            teacherId: teacherData.teacherId,
+            designation: teacherData.designation,
+            emailId: teacherData.emailId,
+            firstName: teacherData.firstName,
+            lastName: teacherData.lastName,
+            mobileNo: teacherData.mobileNo,
+            classTeacher: teacherData.classTeacher,
+            transportSlab: teacherData.transportSlab,
+            personalDetails: teacherData?.personalDetailsData,
+            addressDetails:teacherData?.addressDetailsData,
+            salaryDetails: teacherData?.salaryDetailsData,
+            experienceDetails: teacherData?.experienceDetailsData,
+            createdAt: serverTimestamp(),
+        });
 
-    try {
-      await addDoc(personalDetailsRef, teacherData?.personalDetailsData);
+        console.log("data added succfully");
+        return {
+            status: true,
+            message: "Teacher and subcollections added successfully",
+        };
     } catch (error) {
-      console.error("Error adding AddressDetails: ", error);
+        console.log(error);
+        return {
+            status: false,
+            message: "Error adding teacher and subcollections",
+        };
     }
-
-    try {
-      await addDoc(salaryDetailsRef, teacherData?.salaryDetailsData);
-    } catch (error) {
-      console.error("Error adding AddressDetails: ", error);
-    }
-
-    try {
-      await addDoc(experienceDetailsRef, teacherData?.experienceDetailsData);
-    } catch (error) {
-      console.error("Error adding AddressDetails: ", error);
-    }
-    console.log("data added succfully");
-    return {
-      status: true,
-      message: "Teacher and subcollections added successfully",
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      status: false,
-      message: "Error adding teacher and subcollections",
-    };
-  }
 };
 
 export const getTeacherFromDatabase = async () => {
-  const teacherRef = collection(db, "AddTeachers");
+    const teacherRef = collection(db, "AddTeachers");
 
-  try {
-    const q = query(teacherRef, orderBy("createdAt", "asc"));
-    const querySnapshot = await getDocs(q);
+    try {
+        const q = query(teacherRef, orderBy("createdAt", "asc"));
+        const querySnapshot = await getDocs(q);
 
-    const teacherData = [];
+        const teacherData = [];
 
-    querySnapshot.forEach(async (doc) => {
-      // let salery = doc.get('SalaryDetails');
+        querySnapshot.forEach(async (doc) => {
+            const data = doc.data();
+            if (data.classTeacher) {
+                data.classTeacher = data.classTeacher.join(", "); // Join the array with a comma and space
+            }
 
-      const data = doc.data();
-      if (data.classTeacher) {
-        data.classTeacher = data.classTeacher.join(", "); // Join the array with a comma and space
-      }
+            const modifiedTeacherData = {
+                id: doc.id,
+                Name: data.firstName + " " + data.lastName,
+                "Mobile No": data.mobileNo,
+                Email: data.emailId,
+                Salary: data.salaryDetails?.salaryAmount,
+                "Class Teacher": data.classTeacher || [],
+                "Classes Assigned": ["4A, ", "6B, ", "5C"],
+                "Employee Id": data.teacherId,
+                
+            };
 
-      const modifiedTeacherData = {
-        id: doc.id,
-        Name: data.firstName + " " + data.lastName,
-        "Mobile No": data.mobileNo,
-        Email: data.emailId,
-        Salary: 1000,
-        "Class Teacher": data.classTeacher || [],
-        "Classes Assigned": ["4A, ", "6B, ", "5C"],
-        "Employee Id": data.teacherId,
-      };
+            teacherData.push(modifiedTeacherData);
+        });
 
-      teacherData.push(modifiedTeacherData);
-    });
-
-    return teacherData;
-  } catch (error) {
-    console.error(error);
-  }
+        return teacherData;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
-// export const updateSubjectInDatabase = async (documentId, updatedTeacherData) => {
-//     const teachersRef = collection(db, "teachers");
-//     const teacherDocRef = doc(teachersRef, documentId); // Use Id to reference the specific document
+export const updateTeacherInDatabase = async (documentId, updatedTeacherData) => {
 
-//     try {
-//         await updateDoc(teacherDocRef, updatedTeacherData);
-//         console.log("Document successfully updated!");
-//         return { status: true, message: "Document successfully updated" };
-//     } catch (error) {
-//         console.error("Error updating document:", error);
-//         return { status: false, message: "Error updating document" };
-//     }
-// };
+    const teacherDocRef = doc(db, "AddTeachers", documentId);
+
+    try {
+        const teacherDocSnapshot = await getDoc(teacherDocRef);
+        const existingData = teacherDocSnapshot.data();
+        
+        const teacherDataChanged = JSON.stringify(existingData) !== JSON.stringify(updatedTeacherData);
+        console.log("frontend data",updatedTeacherData);
+        console.log("backend data",existingData);
+        console.log("checking for changes", teacherDataChanged);
+        
+
+        if (teacherDataChanged) {
+            await updateDoc(teacherDocRef, updatedTeacherData);   
+        }
+    }
+    catch (error) {
+        console.error("Error updating document:", error);
+        return { status: false, message: "Error updating document" };
+    }
+};
 
 export const deleteTeacher = async (teacheId) => {
     const teachersRef = collection(db, "AddTeachers");
@@ -207,29 +182,19 @@ export const deleteTeacher = async (teacheId) => {
     }
 };
 
-// export const getTeacherFromDatabase = async (DocId) => {
-//     try {
-//       const teacherDocRef = doc(db, "teachers", DocId);
-//       const teacherDocSnapshot = await getDoc(teacherDocRef);
+export const getTeacherDataFromDd = async (DocId) => {
+    try {
+      const teacherDocRef = doc(db, "AddTeachers", DocId);
+      const teacherDocSnapshot = await getDoc(teacherDocRef);
 
-//       if (teacherDocSnapshot.exists()) {
-//         return teacherDocSnapshot.data();
-//       } else {
-//         return null;
-//       }
-//     } catch (error) {
-//       console.error("Error fetching subject data", error);
-//       throw error;
-//     }
-//   };
-
-//   const personalDetailsData = {
-//     dob: new Date(2016, 0, 5), // Date of birth
-//     fatherName: "xyz",
-//     motherName: "abc",
-//     spouseName: "sqrt",
-//     sex: "male", // Replace with the actual sex value
-//     cast: "Some Cast", // Replace with the actual cast
-//     castCategory: "General", // Replace with the actual cast category
-//     bloodGroup: "AB+", // Replace with the actual blood group
-//   };
+      if (teacherDocSnapshot.exists()) {
+        console.log(teacherDocSnapshot.data())
+        return teacherDocSnapshot.data();
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error("Error fetching subject data", error);
+      throw error;
+    }
+  };
